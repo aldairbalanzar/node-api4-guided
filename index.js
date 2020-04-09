2034d3880820f5cd5c1d041afd49b1f5eb3927a7
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-04-09 13:51:31 -0400

    fixed message error

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
+  const message = process.env.MESSAGE || "hello from localhost"
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
